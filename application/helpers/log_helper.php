8711a7316e093cd6e24b0175455c9cbcaddf578f
xiecheng328 <13136666328@163.com>
2018-05-25 09:49:36 +0800

    修改日志时区问题

--- application/helpers/log_helper.php
+++ application/helpers/log_helper.php
@@ -1,6 +1,8 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 	function insert_log_data($id,$do,$genre){
+
+        date_default_timezone_set('PRC');
 		$data = array(
                 'u_id'=>$id,'do'=>$do,
                 'time'=>date('Y-m-d H:i:s'),'genre'=>$genre
